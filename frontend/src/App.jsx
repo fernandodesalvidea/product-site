a1c33d494f025b4205ccecc0cccae6ce49e4173d
ferLaika <fedesalvideal@outlook.com>
2025-11-13 14:45:14 -0600

    fix file path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Box, useColorMode, useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage.jsx";
-import CreatePage from "../pages/CreatePage.jsx";
-import Navbar from "../components/Navbar.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
